86e2d445f27bb66742b00f51c397ff86330f8d0d
Hitori <77605422+hitori839@users.noreply.github.com>
2026-05-18 12:10:16 +0000

    Event practice

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,3 +1,9 @@
-const hellos = document.querySelectorAll(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
-console.log(hellos)
+//클릭했을 때 반응할 이벤트
+function handleTitleClick(){
+    title.style.color = "blue";
+}
+
+// click 했을 때 이벤트 설정 및 실행 함수 설정
+title.addEventListener("click", handleTitleClick);
