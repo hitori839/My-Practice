2e271ea4714c9c167be1187f3d56ed317899183a
Hitori <77605422+hitori839@users.noreply.github.com>
2026-05-19 11:43:44 +0000

    practice 26.05.19

--- JS/momentum/app.js
+++ JS/momentum/app.js
@@ -1,9 +1,14 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
-//클릭했을 때 반응할 이벤트
 function handleTitleClick(){
-    title.style.color = "blue";
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-// click 했을 때 이벤트 설정 및 실행 함수 설정
-title.addEventListener("click", handleTitleClick);
+h1.addEventListener("click", handleTitleClick); 
